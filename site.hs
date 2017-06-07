668191dcdcfce68f7ad4c1510c5aa739e10ff61b
Andreas Farre <farre@mozilla.com>
2017-06-07 12:21:00 +0200

    Unfortunately giving up on tex. Pandoc lacks features :(

--- site.hs
+++ site.hs
@@ -1,17 +1,7 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings, TupleSections #-}
-import Data.List (sortBy)
-import Data.Maybe
 import Data.Monoid ((<>))
-import Data.Ord
-import Data.Time.Clock
-import Data.Time.Format
-import Control.Monad
 import Hakyll
-
-import Debug.Trace
-import Text.Pandoc.Definition
-import Text.Pandoc.Options
 
 --------------------------------------------------------------------------------
 main :: IO ()
@@ -28,153 +18,55 @@ main = hakyll $ do
         route   idRoute
         compile copyFileCompiler
 
-    match "posts/*.tex" $ do
+    match "posts/*" $ do
         route $ setExtension "html"
+        let ctx = getContext [] ""
         compile $ do
-          let extendedPostCtx =
-                infoCtx <>
-                postCtx
-          myPandocCompiler
-            >>= loadAndApplyTemplate "templates/post.html" extendedPostCtx
-            >>= loadAndApplyTemplate "templates/default.html" extendedPostCtx
+          pandocCompiler
+            >>= loadAndApplyTemplate "templates/post.html" ctx
+            >>= loadAndApplyTemplate "templates/default.html" ctx
             >>= relativizeUrls
 
     create ["archive.html"] $ do
         route idRoute
         compile $ do
-            posts <- myRecentFirst =<< loadAll "posts/*"
-            let archiveCtx =
-                    listField "posts" postCtx (return posts) <>
-                    constField "title" "Archives"            <>
-                    infoCtx                                  <>
-                    myDefaultContext
+            posts <- recentFirst =<< loadAll "posts/*"
+            let ctx = getContext posts "Archives"
 
             makeItem ""
-                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+                >>= loadAndApplyTemplate "templates/archive.html" ctx
+                >>= loadAndApplyTemplate "templates/default.html" ctx
                 >>= relativizeUrls
 
     match (fromList ["about.md", "contact.md"]) $ do
-      compile $ myPandocCompiler
-        >>= loadAndApplyTemplate "templates/plain.html" postCtx
+      compile $ let ctx = getContext [] "" in
+        pandocCompiler
+        >>= loadAndApplyTemplate "templates/plain.html" ctx
         >>= relativizeUrls
 
     match "index.html" $ do
         route idRoute
         compile $ do
-            posts <- myRecentFirst =<< loadAll "posts/*"
-            let indexCtx =
-                    listField "posts" postCtx (return posts) <>
-                    constField "title" "Home"                <>
-                    infoCtx                                  <>
-                    myDefaultContext
+            posts <- recentFirst =<< loadAll "posts/*"
+            let ctx = getContext posts "Home"
 
             getResourceBody
-                >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/default.html" indexCtx
+                >>= applyAsTemplate ctx
+                >>= loadAndApplyTemplate "templates/default.html" ctx
                 >>= relativizeUrls
 
     match "templates/*" $ compile templateBodyCompiler
 
 --------------------------------------------------------------------------------
-postCtx :: Context String
-postCtx =
-  pandocField "date" <>
-  pandocField "title" <>
-  dateField "date" "%B %e, %Y" <>
-  myDefaultContext
-
-infoCtx :: Context String
-infoCtx =
-  field "about" (\_ -> loadBody (fromFilePath "about.md"))     <>
-  field "contact" (\_ -> loadBody (fromFilePath "contact.md"))
-
-myDefaultContext :: Context String
-myDefaultContext =
-  metadataField <>
-  constField "keywords" "" <>
-  defaultContext
-
-pandocField :: String -> Context a
-pandocField f = field f $ \item -> do
-  date <- loadSnapshot (itemIdentifier item) f
-  return (itemBody date)
-
-getPandocMeta :: Pandoc -> Meta
-getPandocMeta (Pandoc meta _) = meta
-
-fromDocDate :: Pandoc -> Maybe String
-fromDocDate doc = do
-  d <- listToMaybe [ date | (Str date) <- docDate (getPandocMeta doc)]
-  t <- msum [ parseTime' format d | format <- formats ]
-  return $ formatTime defaultTimeLocale (iso8601DateFormat Nothing) t
+getContext :: [Item String] -> String -> Context String
+getContext posts title =
+  metadataField                                                <>
+  listField "posts" ctx (return posts)                         <>
+  ctx
   where
-    parseTime' :: String -> String -> Maybe UTCTime
-    parseTime' = parseTimeM True defaultTimeLocale
-    formats    =
-        [ "%a, %d %b %Y %H:%M:%S %Z"
-        , "%Y-%m-%dT%H:%M:%S%Z"
-        , "%Y-%m-%d %H:%M:%S%Z"
-        , "%Y-%m-%d"
-        , "%B %e, %Y %l:%M %p"
-        , "%B %e, %Y"
-        , "%b %d, %Y"
-        ]
-
-fromDocTitle :: Pandoc -> Maybe String
-fromDocTitle doc =
-  case unwords [ title | (Str title) <- docTitle (getPandocMeta doc)] of
-    "" -> Nothing
-    t  -> Just t
-
-savePandocData :: Pandoc -> (Pandoc -> Maybe String) -> String -> Compiler ()
-savePandocData doc fn field = do
-  identifier <- getUnderlying
-  metadata <- getMetadataField identifier field
-  case msum [fn doc, metadata] of
-    Nothing -> return ()
-    Just value -> do
-      item <- makeItem value
-      _ <- saveSnapshot field item
-      return ()
-
-myPandocCompiler :: Compiler (Item String)
-myPandocCompiler = pandocCompilerWithTransformM ropt wopt $ \doc -> do
-  savePandocData doc fromDocDate "date"
-  savePandocData doc fromDocTitle "title"
-  return doc
-  where
-    ropt = defaultHakyllReaderOptions
-    wopt = defaultHakyllWriterOptions
-      {
-        writerHTMLMathMethod = MathML (Just ""),
-        writerListings = True
-      }
-
-myChronological :: [Item a] -> Compiler [Item a]
-myChronological = sortByM getTime
-  where
-    sortByM :: (Monad m, Ord k) => (a -> m k) -> [a] -> m [a]
-    sortByM f xs = liftM (map fst . sortBy (comparing snd)) $
-                   mapM (\x -> liftM (x,) (f x)) xs
-    getTime :: Item a -> Compiler (Maybe UTCTime)
-    getTime item = do
-      date <- loadSnapshot (itemIdentifier item) "date"
-      return $ case itemBody date of
-                 "" -> Nothing
-                 d -> (msum [ parseTime' format d | format <- formats])
-
-    parseTime' :: String -> String -> Maybe UTCTime
-    parseTime' = parseTimeM True defaultTimeLocale
-    formats    =
-        [ "%a, %d %b %Y %H:%M:%S %Z"
-        , "%Y-%m-%dT%H:%M:%S%Z"
-        , "%Y-%m-%d %H:%M:%S%Z"
-        , "%Y-%m-%d"
-        , "%B %e, %Y %l:%M %p"
-        , "%B %e, %Y"
-        , "%b %d, %Y"
-        ]
-
-myRecentFirst :: [Item a] -> Compiler [Item a]
-myRecentFirst = liftM reverse . myChronological
+    ctx =
+      metadataField                                                <>
+      constField "title" title                                     <>
+      field "about" (\_ -> loadBody (fromFilePath "about.md"))     <>
+      field "contact" (\_ -> loadBody (fromFilePath "contact.md")) <>
+      defaultContext
