bd2691976f39c5d96691c4d937e35c07258084f4
Andreas Farre <farre@mozilla.com>
2017-06-13 11:46:08 +0200

    Publish: Starting a blog

---
+++ js/comments.js
@@ -0,0 +1,63 @@
+function loadComments(commentId) {
+  var id = commentId;
+  return new Promise(function(resolve, reject) {
+    let url = ["https://api.github.com/repos/farre/blog/issues", id, "comments"].join('/')
+    var request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.setRequestHeader("Accept", "application/vnd.github.full+json");
+    request.responseType = 'json';
+    request.onload = function() {
+      if (request.status === 200) {
+        resolve(request.response);
+      } else {
+        reject(Error('Failed to load: ' + request.statusText));
+      }
+    };
+    request.onerror = function() {
+      reject(Error('There was a network error'));
+    };
+
+    request.send();
+  });
+}
+
+function populateComments(receiver) {
+  if (!receiver) {
+    return;
+  }
+
+  loadComments(receiver.dataset.id).then(comments => {
+    var elementTemplate = document.getElementById('comment-template');
+    var imageTemplate = document.getElementById('image-template');
+    for (let comment of comments) {
+      let user = comment.user;
+
+      let element = elementTemplate.cloneNode(true);
+      let image = imageTemplate.cloneNode(false);
+      image.src = user.avatar_url;
+      image.id = "";
+
+      let name = element.querySelector("a.commentuser");
+      name.href = user.html_url;
+      name.innerText = user.login;
+      let date = element.querySelector("a.commentdate");
+      date.href = comment.html_url;
+      date.innerText = (new Date(comment.created_at)).toUTCString();
+
+      element.querySelector("div.commentgravatar").appendChild(image);
+      element.querySelector("div.commentbody").innerHTML = comment.body_html;
+      element.id = "";
+
+      receiver.appendChild(element);
+    }
+  }).catch(error => {
+    console.log(error);
+  });
+}
+
+function commentHandler(event) {
+  var comments = document.getElementById('comments');
+  populateComments(comments);
+}
+
+addEventListener('load', commentHandler)
